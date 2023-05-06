53bf0013f796f8c0b079e3d76e92672e71766e3d
tamphongtrinh1991 <tamphongtrinh1991@gmail.com>
2023-05-06 14:55:42 +0700

    chua xong dau

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,45 +1,22 @@
 <?php
 
 namespace App\Providers;
-
-use App\Repositories\Api\Product\ApiProductRepository;
-use App\Repositories\Api\Product\ApiProductRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
-use App\Repositories\Category\CategoryRepository;
-use App\Repositories\Category\CategoryRepositoryInterface;
 
-use App\Services\Category\CategoryService;
-use App\Services\Category\CategoryServiceInterface;
+/* CategoryService */
+use App\Services\Interfaces\CategoryServiceInterface;
+use App\Services\CategoryService;
 
-use App\Repositories\Category\ProductRepositoryInterface;
-use App\Repositories\Customer\CustomerRepository;
-use App\Repositories\Customer\CustomerRepositoryInterface;
-use App\Repositories\Product\ProductRepository;
-use App\Repositories\Product\ProductRepositoryInterface as ProductProductRepositoryInterface;
-use App\Services\Product\ProductService;
-use App\Services\Product\ProductServiceInterface;
-
-use App\Repositories\Group\GroupRepository;
-use App\Repositories\Group\GroupRepositoryInterface;
-use App\Repositories\Order\OrderRepository;
-use App\Repositories\Order\OrderRepositoryInterface;
-use App\Services\Customer\CustomerService;
-use App\Services\Customer\CustomerServiceInterface;
-
-use App\Repositories\User\UserRepository;
-use App\Repositories\User\UserRepositoryInterface;
-use App\Services\Api\Product\ApiProductService;
-use App\Services\Api\Product\ApiProductServiceInterface;
-use App\Services\Group\GroupService;
-use App\Services\Group\GroupServiceInterface;
-use App\Services\Order\OrderService;
-use App\Services\Order\OrderServiceInterface;
-use App\Services\User\UserService;
-use App\Services\User\UserServiceInterface;
+/* CategoryRepository */
+use App\Repositories\Interfaces\CategoryRepositoryInterface;
+use App\Repositories\Eloquents\CategoryRepository;
 
 
 
-use Illuminate\Pagination\Paginator;
+
+
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -50,32 +27,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // đăng ký category
-        $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
-        $this->app->bind(CategoryServiceInterface::class, CategoryService::class);
+ 
+/*
+            Các interface không thể dùng để khởi tạo đối tượng
+            Binding interface với một lớp giúp chúng ta có thể dùng được
+            Tắt dòng binding là thấy tai hại liền :)
+        */
+        /* Binding Services*/
+        $this->app->singleton(CategoryServiceInterface::class, CategoryService::class);
 
-        // đăng ký product
-        $this->app->bind(ProductProductRepositoryInterface::class, ProductRepository::class);
-        $this->app->bind(ProductServiceInterface::class, ProductService::class);
 
-        // đăng ký group
-        $this->app->bind(GroupRepositoryInterface::class, GroupRepository::class);
-        $this->app->bind(GroupServiceInterface::class, GroupService::class);
-
-        // đăng ký customer
-        $this->app->bind(CustomerRepositoryInterface::class, CustomerRepository::class);
-        $this->app->bind(CustomerServiceInterface::class, CustomerService::class);
-
-        // đăng ký user
-        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
-        $this->app->bind(UserServiceInterface::class, UserService::class);
-        // đăng ký order
-        $this->app->bind(OrderRepositoryInterface::class, OrderRepository::class);
-        $this->app->bind(OrderServiceInterface::class, OrderService::class);
-         // đăng ký Api products
-         $this->app->bind(ApiProductRepositoryInterface::class, ApiProductRepository::class);
-         $this->app->bind(ApiProductServiceInterface::class, ApiProductService::class);
+        
+        /* Binding Repositories*/
+        $this->app->singleton(CategoryRepositoryInterface::class, CategoryRepository::class);
+        
     }
+
     /**
      * Bootstrap any application services.
      *
@@ -83,7 +50,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::useBootstrapFive();
-        Paginator::useBootstrapFour();
+        //
     }
 }
