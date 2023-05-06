d423f8acc72a8ca9a709be0b616883b05944a767
Nguyenvannho <nho38443@gmail.com>
2023-05-06 08:47:59 +0700

    chua hoan thanh product

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +26,14 @@ Route::get('/', function () {
 
 Route::get('/dasboard', function () {
     return view('admin.layouts.master');
+});
+Route::prefix('product')->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('product.index');
+    Route::get('/create', [ProductController::class, 'create'])->name('product.create');
+    Route::post('/store', [ProductController::class, 'store'])->name('product.store');
+    Route::get('/show/{id}', [ProductController::class, 'show'])->name('product.show');
+    Route::get('/edit/{id}', [ProductController::class, 'edit'])->name('product.edit');
+    Route::put('/update/{id}', [ProductController::class, 'update'])->name('product.update');
+    Route::delete('/destroy/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+    Route::get('/search', [ProductController::class, 'search'])->name('product.search');
 });
