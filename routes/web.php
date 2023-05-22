084aafbdf3c9c6714e1df1157675194e662cd07d
tamphongtrinh1991 <tamphongtrinh1991@gmail.com>
2023-05-22 08:44:38 +0700

    chua xong

--- routes/web.php
+++ routes/web.php
@@ -71,8 +71,8 @@ Route::resource('groups', GroupController::class);
 Route::get('customers/search', [CustomerController::class, 'search'])->name('customers.search');
 Route::resource('customers', CustomerController::class);
 // Đây là Orderssearch
-Route::get('orders/search', [OrderController::class, 'search'])->name('orders.search');
-Route::get('orders/xuat', [OrderController::class, 'exportOrder'])->name('orders.xuat');
-Route::resource('orders', OrderController::class);
+// Route::get('orders/search', [OrderController::class, 'search'])->name('orders.search');
+// Route::get('orders/xuat', [OrderController::class, 'exportOrder'])->name('orders.xuat');
+// Route::get('orders/show{id}', OrderController::class,'show')->name('orders.order_detail');
 });
 
