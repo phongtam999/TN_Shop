c540d088cf831d8c0a59efc6168f4444cf7960ef
Nguyenvannho <nho38443@gmail.com>
2023-05-22 08:43:16 +0700

    hoan thanh goi email password

--- config/mail.php
+++ config/mail.php
@@ -40,7 +40,7 @@ return [
             'port' => 587,
             'encryption' => 'tls',
             'username' => 'nho230204@gmail.com',
-            'password' => 'smfdflhgioapbscz',
+            'password' => 'anbigjpwlvvflcym',
             'timeout' => null,
             'local_domain' => 'your-mailgun-domain',
         ],
--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -12,7 +12,10 @@
                 <li class="icons dropdown">
                     <div class="user-img c-pointer position-relative" data-toggle="dropdown">
                         <span class="activity active"></span>
-                        <img src="{{asset(Auth()->user()->image)}}" height="40" width="40" alt="">
+                        @if(Auth::check() && Auth::user()->image)
+                            <img src="{{ asset(Auth::user()->image) }}" height="40" width="40" alt="">
+                        @endif
+
                     </div>
                     <div class="drop-down dropdown-profile   dropdown-menu">
                         <div class="dropdown-content-body">
