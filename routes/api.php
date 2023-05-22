43bd10b5c59c865036ac570c7d18d2ddc4b25554
Nguyenvannho <nho38443@gmail.com>
2023-05-22 11:40:04 +0700

    comlete login

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,13 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\Api\ProductController;
-// use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\UserController;
-// use App\Http\Controllers\Api\CustomerController;
-// use App\Http\Controllers\Api\GroupController;
-// use App\Http\Controllers\APIController;
+use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\GroupController;
 
 
 /*
@@ -30,19 +29,19 @@ Route::controller(AuthController::class)->group(function () {
 
 });
 
-// Route::Resource('products',ProductController::class);
-// Route::get('carts',[CartController::class,'getAllCart']);
-// Route::get('products',[ProductController::class,'index']);
-// Route::post('add_to_cart',[CartController::class,'addToCart']);
-// Route::put('update_cart',[CartController::class,'update']);
-// // Route::delete('remove_cart',[CartController::class,'remove']);
-// Route::delete('carts/remove_cart/{id}',[CartController::class,'removeToCart']);
-// Route::get('category_list',[ProductController::class,'category_list']);
-// Route::get('products/{id}',[ProductController::class,'product_detail']);
+Route::resource('products',ProductController::class);
+Route::get('carts',[CartController::class,'getAllCart']);
+Route::get('products',[ProductController::class,'index']);
+Route::post('add_to_cart',[CartController::class,'addToCart']);
+Route::put('update_cart',[CartController::class,'update']);
+// Route::delete('remove_cart',[CartController::class,'remove']);
+Route::delete('carts/remove_cart/{id}',[CartController::class,'removeToCart']);
+Route::get('category_list',[ProductController::class,'category_list']);
+Route::get('products/{id}',[ProductController::class,'product_detail']);
 
-// Route::apiResource('categories',CategoryController::class);
-// Route::apiResource('products',ProductController::class);
-// Route::apiResource('users',UserController::class);
-// Route::apiResource('customers',CustomerController::class);
-// Route::apiResource('groups',GroupController::class);
+Route::apiResource('categories',CategoryController::class);
+Route::apiResource('products',ProductController::class);
+Route::apiResource('users',UserController::class);
+Route::apiResource('customers',CustomerController::class);
+Route::apiResource('groups',GroupController::class);
 
