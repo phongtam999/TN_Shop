a3ed5a1a6535035dfa28e15c6426570a7073f6bf
Nguyenvannho <nho38443@gmail.com>
2023-05-22 14:49:13 +0700

    feature auth jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,18 +19,98 @@ class AuthController extends Controller
  
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login','register']]);
+        // $this->middleware('auth:api', ['except' => ['login','register']]);
     }
 
     
 
+    public function login()
+    {
+       
+        if (Auth::check()) {
+            return redirect()->route('dashboard');
+        } else {
+            return view('admin.mails.login');
+        }
+    }
+
+    public function postLogin(Request $request)
+    {
+       
+        $messages = [
+            "email.exists" => "Email không đúng định dạng",
+            "password.exists" => "Mật khẩu không đúng",
+        ];
+        $validator = Validator::make($request->all(), [
+            'email' => 'exists:users,email',
+            'password' => 'exists:users,password',
+        ], $messages);
+        $data = $request->only('email', 'password');
+        if (Auth::attempt($data)) {
+            session()->flash('success', 'Đăng nhập thành công!');
+            return redirect()->route('dashboard');
+        } else {
+            return back()->withErrors($validator)->withInput();
+        }
+    
+    }
+
+    
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+    public function forgot_password()
+    {
+        return view('admin.mails.forgot_password');
+    }
+    public function post_forgot_password(Request $request)
+    {
+        $customer = User::where('email', $request->email)->first();
+        
+        if ($customer) {
+            $pass = Str::random(6);
+            $customer->password = bcrypt($pass);
+            $customer->save();
+            
+            $data = [
+                'name' => $customer->name,
+                'pass' => $pass,
+                'email' => $customer->email,
+            ];
+            try {
+                // dd($customer);
+                // dd($data);
+                Mail::send('admin.mails.password', compact('data'), function ($email) use ($customer) {
+                    $email->subject('Shop TN');
+                    $email->to($customer->email, $customer->name);
+                });
+    
+                // Gửi email thành công
+                $successMessage = 'Email đã được gửi thành công!';
+                return redirect()->route('login')->with('success', $successMessage);
+            } catch (\Exception $e) {
+                // Gửi email không thành công
+                $errorMessage = 'Đã xảy ra lỗi khi gửi email.';
+                return redirect()->route('login')->with('error', $errorMessage);
+            }
+        } else {
+            // Email không tồn tại
+            $errorMessage = 'Email không tồn tại.';
+            return redirect()->route('login')->with('error', $errorMessage);
+        }
+    }
+
+
+    
     // public function login()
     // {
        
     //     if (Auth::check()) {
     //         return redirect()->route('dashboard');
     //     } else {
-    //         return view('admin.Auth.login');
+    //         return view('admin.mails.login');
     //     }
     // }
 
@@ -63,7 +143,7 @@ class AuthController extends Controller
     // }
     // public function forgot_password()
     // {
-    //     return view('admin.Auth.forgot_password');
+    //     return view('admin.mails.forgotpass');
     // }
     // public function post_forgot_password(Request $request)
     // {
@@ -82,7 +162,7 @@ class AuthController extends Controller
     //         try {
     //             // dd($customer);
     //             // dd($data);
-    //             Mail::send('admin.Auth.password', compact('data'), function ($email) use ($customer) {
+    //             Mail::send('admin.mails.password', compact('data'), function ($email) use ($customer) {
     //                 $email->subject('Shop TN');
     //                 $email->to($customer->email, $customer->name);
     //             });
@@ -101,84 +181,4 @@ class AuthController extends Controller
     //         return redirect()->route('login')->with('error', $errorMessage);
     //     }
     // }
-
-
-    
-    public function login()
-    {
-       
-        if (Auth::check()) {
-            return redirect()->route('dashboard');
-        } else {
-            return view('admin.Auth.login');
-        }
-    }
-
-    public function postLogin(Request $request)
-    {
-       
-        $messages = [
-            "email.exists" => "Email không đúng định dạng",
-            "password.exists" => "Mật khẩu không đúng",
-        ];
-        $validator = Validator::make($request->all(), [
-            'email' => 'exists:users,email',
-            'password' => 'exists:users,password',
-        ], $messages);
-        $data = $request->only('email', 'password');
-        if (Auth::attempt($data)) {
-            session()->flash('success', 'Đăng nhập thành công!');
-            return redirect()->route('dashboard');
-        } else {
-            return back()->withErrors($validator)->withInput();
-        }
-    
-    }
-
-    
-    public function logout()
-    {
-        // Auth::logout();
-        return redirect()->route('login');
-    }
-    public function forgot_password()
-    {
-        return view('admin.mails.forgotpass');
-    }
-    public function post_forgot_password(Request $request)
-    {
-        $customer = User::where('email', $request->email)->first();
-        
-        if ($customer) {
-            $pass = Str::random(6);
-            $customer->password = bcrypt($pass);
-            $customer->save();
-            
-            $data = [
-                'name' => $customer->name,
-                'pass' => $pass,
-                'email' => $customer->email,
-            ];
-            try {
-                // dd($customer);
-                // dd($data);
-                Mail::send('admin.Auth.password', compact('data'), function ($email) use ($customer) {
-                    $email->subject('Shop TN');
-                    $email->to($customer->email, $customer->name);
-                });
-    
-                // Gửi email thành công
-                $successMessage = 'Email đã được gửi thành công!';
-                return redirect()->route('login')->with('success', $successMessage);
-            } catch (\Exception $e) {
-                // Gửi email không thành công
-                $errorMessage = 'Đã xảy ra lỗi khi gửi email.';
-                return redirect()->route('login')->with('error', $errorMessage);
-            }
-        } else {
-            // Email không tồn tại
-            $errorMessage = 'Email không tồn tại.';
-            return redirect()->route('login')->with('error', $errorMessage);
-        }
-    }
 }
