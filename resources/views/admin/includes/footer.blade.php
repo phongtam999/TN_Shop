ab7880bf1191cd83cfa42af08c24619836e6a99b
tamphongtrinh1991 <tamphongtrinh1991@gmail.com>
2023-05-30 16:53:10 +0700

    hoan thanh du an m6

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -1,7 +1,7 @@
 
 <div class="footer">
     <div class="copyright">
-        <p>Copyright &copy; Designed & Developed by <a href="https://themeforest.net/user/quixlab">Quixlab</a>
+        <p>Bản quyền &copy; Thiết kế & Phát triển bởi <a href="https://themeforest.net/user/quixlab">Quixlab</a>
             2018</p>
     </div>
 </div>
